00e94164d1e9a0f115ba5ce1349ec0a89250cec3
Butz, Luis <LButz@bhs-world.com>
2026-03-16 14:08:55 +0100

    renamed project to workTimeTracker

--- src/workTimeTracker/CurrentWorkDay.cs
+++ src/workTimeTracker/CurrentWorkDay.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace src
+namespace workTimeTracker
 {
     public class CurrentWorkDay
     {
--- src/workTimeTracker/Program.cs
+++ src/workTimeTracker/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace src
+﻿namespace workTimeTracker
 {
     internal class Program
     {
